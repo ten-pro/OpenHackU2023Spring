04b40b2a9337df711196019c604ba5332a0ef119
doraemon123453@gmail.com <doraemon123453@gmail.com>
2023-02-14 02:26:40 +0900

    環境構築とヘッダーとフッター完了

---
+++ todoapp/src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="footer">
+        <img class="footer_icon" src="./PNG/home.png"/>
+        <img class="footer_icon" src="./PNG/todo.png"/>
+        <img class="footer_icon" src="./PNG/post.png"/>
+        <img class="footer_icon" src="./PNG/group.png"/>
+        <img class="footer_icon" src="./PNG/sns.png"/>
+    </div>
+</template>
+<script setup lang="ts">
+</script>
+<style scoped>
+.footer{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap:3vw;
+    position:absolute;
+    width:90vw;
+    height:8vh;
+    bottom:2vh;
+    left:4.7vw;
+    border-radius: 50px;
+    border: solid 0.7vw black;
+    background-color: #33CFC6;
+}
+.footer_icon{
+    width:12vw;
+    height:12vw;
+}
+</style>
+  
---
+++ todoapp/src/components/Hello.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+    </div>
+  </template>
+  <script setup lang="ts">
+  // import HelloWorld from './components/HelloWorld.vue'
+  </script>
+  <style scoped>
+  
+  </style>
+  
