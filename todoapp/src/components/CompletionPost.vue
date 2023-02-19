6a7d141f26ab0ada17785bf64cd2c62dea1d8a15
doraemon123453@gmail.com <doraemon123453@gmail.com>
2023-02-19 23:39:09 +0900

    画像アップロード完了＆CSS途中

--- todoapp/src/components/CompletionPost.vue
+++ todoapp/src/components/CompletionPost.vue
@@ -1,6 +1,15 @@
 <template>
-    <input type="file" ref="fileInput">
-    <button @click="uploadFile">Upload</button>
+  <div class="return">
+    <img class="left" src="./PNG/left.png" />
+  </div>
+  <div class="image_area">
+    <div class="title">チャーハン材料</div>
+    <div class="subtitle">完了条件</div>
+    <div class="text">ネギ、卵、ベーコンを買ってくる</div>
+    <div class="subtitle select">画像選択</div>
+    <input type="file" ref="fileInput" class="">
+  </div>
+  <button @click="uploadFile">Upload</button>
 </template>
 <script setup lang="ts">
 import axios from "axios"
@@ -16,8 +25,8 @@ const uploadFile = () => {
   // create FormData object and append file object
   const formData = new FormData();
   formData.append('image', file);
-  formData.append('user_id', "1");
-    formData.append('todo_id', "1");
+  formData.append('user_id', 4);
+    formData.append('todo_id', 1);
 
   // send FormData object to php using axios post method
   axios.post('http://mp-class.chips.jp/group_task/upload.php',formData)
@@ -54,10 +63,41 @@ defineExpose({
 });
 </script>
 <style scoped>
-.childwrap{
-    width:40vw;
-    height:10vh;
-    margin-right:20vw;
-    background-color: rgb(255, 0, 0);
+.return{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width:30vw;
+  height:6vh;
+  background-color: white;
+  border: solid 2px #5AB4BD;
+  border-radius: 20px;
+}
+.left{
+  width:15vw;
+  height:4.5vh;
+}
+.image_area{
+  width:90vw;
+  margin:auto;
+  background-color: white;
+  border: solid 2px #5AB4BD;
+  border-radius: 10px;
+}
+.title{
+  font-size: 4.5vh;
+  text-align: center;
+  font-weight: bold;
+}
+.subtitle{
+  font-size:3vh;
+  text-align: center;
+  font-weight: bold;
+}
+.text{
+  text-align: center;
+}
+.select{
+  font-size:2.5vh;
 }
 </style>
