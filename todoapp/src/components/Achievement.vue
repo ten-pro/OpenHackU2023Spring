906bb9770f5a2e290ad6308d1a1fc094a5ae4895
ueda masanari <Doraemon123453@gmail.com>
2023-02-20 03:22:37 +0900

    実績とヘッダー完成

--- todoapp/src/components/Achievement.vue
+++ todoapp/src/components/Achievement.vue
@@ -1,18 +1,112 @@
 <template>
     <div class="wrap">
-        <OneAchievement />
+        <div class="flex">
+            <div v-for="(item, index) in itirans" :key="index" class="flex_none">
+                <div  class="childwrap">
+                    <div class="title">{{ item.title }}</div>
+                    <div class="text">{{ item.text }}</div>
+                </div>
+                <img v-if="index!=itirans.length - 1" src="./PNG/right.png" class="yajirusi"/>
+            </div>
+        </div>
+        <div class="flex">
+            <div v-for="(item, index) in itirans" :key="index" class="flex_none">
+                <div  class="childwrap">
+                    <div class="title">{{ item.title }}</div>
+                    <div class="text">{{ item.text }}</div>
+                </div>
+                <img v-if="index!=itirans.length - 1" src="./PNG/right.png" class="yajirusi"/>
+            </div>
+        </div>
+        <div class="flex">
+            <div v-for="(item, index) in itirans" :key="index" class="flex_none">
+                <div  class="childwrap">
+                    <div class="title">{{ item.title }}</div>
+                    <div class="text">{{ item.text }}</div>
+                </div>
+                <img v-if="index!=itirans.length - 1" src="./PNG/right.png" class="yajirusi"/>
+            </div>
+        </div>
+        <div class="flex">
+            <div v-for="(item, index) in itirans" :key="index" class="flex_none">
+                <div  class="childwrap">
+                    <div class="title">{{ item.title }}</div>
+                    <div class="text">{{ item.text }}</div>
+                </div>
+                <img v-if="index!=itirans.length - 1" src="./PNG/right.png" class="yajirusi"/>
+            </div>
+        </div>
+        <div class="flex">
+            <div v-for="(item, index) in itirans" :key="index" class="flex_none">
+                <div  class="childwrap">
+                    <div class="title">{{ item.title }}</div>
+                    <div class="text">{{ item.text }}</div>
+                </div>
+                <img v-if="index!=itirans.length - 1" src="./PNG/right.png" class="yajirusi"/>
+            </div>
+        </div>
+        <div class="flex">
+            <div v-for="(item, index) in itirans" :key="index" class="flex_none">
+                <div  class="childwrap">
+                    <div class="title">{{ item.title }}</div>
+                    <div class="text">{{ item.text }}</div>
+                </div>
+                <img v-if="index!=itirans.length - 1" src="./PNG/right.png" class="yajirusi"/>
+            </div>
+        </div>
     </div>
 </template>
 <script setup lang="ts">
 import OneAchievement from './achievement/OneAchievement.vue'
+import { reactive } from 'vue';
+
+let itirans = reactive([
+  {title:"aaa",text:"bbb"},
+  {title:"aaa",text:"bbb"},
+  {title:"aaa",text:"bbb"},
+  {title:"aaa",text:"bbb"},
+  {title:"aaa",text:"bbb"},
+  {title:"aaa",text:"bbb"},
+  {title:"aaa",text:"bbb"},
+  {title:"aaa",text:"bbb"},
+  {title:"aaa",text:"bbb"},
+])
 </script>
 <style scoped>
 .wrap{
     width:100vw;
     height:80.5vh;
-    padding: 3vh;
-    background-color: aqua;
-    overflow: auto;
+    overflow:auto;
+    overflow-x:auto;
 }
+.childwrap{
+    min-width:40vw;
+    min-height:10vh;
+    background-color: #33CFC6;
+    border-radius: 20px;
+}
+.yajirusi{
+    width:20vw;
+    height:3vh;
+    margin:auto;
+}
+.flex{
+    display: flex;
+    margin-top:3vh;
+    margin-left:5vw;
+}
+.flex_none{
+    display: flex;
+}
+.title{
+    color:white;
+    text-align: center;
+    font-size: 7vw;
+}
+.text{
+    color:white;
+    text-align: center;
+}
+
 </style>
   
--- todoapp/src/components/Header.vue
+++ todoapp/src/components/Header.vue
@@ -1,18 +1,38 @@
 <template>
     <div class="header">
+        <div class="username">上田匡成</div>
+        <div class="title">１日目</div>
     </div>
 </template>
 <script setup lang="ts">
+import axios from 'axios';
+axios
+    .post('http://mp-class.chips.jp/group_task/main.php', {
+        user_id:1,
+        rank_count: ''
+    }, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    })
+    .then(
+        (response) => (console.log(response.data))
+    )
 </script>
 <style scoped>
 .header{
+    display: flex;
+    justify-content:space-around;
+    align-items: center;
     width:90vw;
     height:6.5vh;
     margin:auto;
     margin-top:2vh;
     border-radius: 50px;
+    font-size:2.5vh;
     border: solid 0.7vw black;
     background-color: #33CFC6;
+    color:white;
 }
 </style>
   
