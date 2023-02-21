a60904a157fd40d387eedd6b7375da1ad4ac4863
ueda masanari <Doraemon123453@gmail.com>
2023-02-22 04:01:23 +0900

    home画面API接続まで完成

--- todoapp/src/components/Home.vue
+++ todoapp/src/components/Home.vue
@@ -36,9 +36,13 @@
 
     <div class="today">
         <div class="title">
-            <div class="task">
-
-            </div>
+            <div class="title_message">{{ todo.title }}</div>
+                <div class="task">
+                    <div class="ran">ランク</div><div class="rank_text">{{ todo.rank }}</div>
+                    <div class="kan">完了条件</div><div class="condition_text">{{ todo.todo_condition }}</div>
+                    <div class="syou">詳細</div>
+                    <div class="message_text">{{ todo.message }}</div>
+                </div>
         </div>
     </div>
 </template>
@@ -57,11 +61,15 @@ let todo = reactive({
     s:2,
     a:2,
     b:2,
-    c:2
+    c:2,
+    title:"",
+    message:"",
+    todo_condition:"",
+    rank:"",
 })
 axios
     .post('http://mp-class.chips.jp/group_task/main.php', {
-        user_id:1,
+        user_id:2,
         rank_count: ''
     }, {
         headers: {
@@ -76,6 +84,10 @@ axios
         todo.a=data[1].count;
         todo.b=data[2].count;
         todo.c=data[3].count;
+        todo.title=data[5].title;
+        todo.message=data[5].messsage;
+        todo.todo_condition=data[5].todo_condition;
+        todo.rank=data[5].rank;
     })
 </script>
 <style scoped>
@@ -172,19 +184,86 @@ axios
     border-radius: 20px;
 }
 .title{
+    position: relative;
     width:40vw;
     height:30vh;
     background-color:#6AD3DD;
     border:solid 0.1vh black;
     border-radius: 20px;
+    margin-left:-0.1vh;
+    margin-top:-0.1vh;
+}
+.title_message{
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width:40vw;
+    height:5vh;
+    color:white;
 }
 .task{
+    position: relative;
+    display:grid;
+    grid-template-rows: 2vh 5vh 5vh 17vh;
+    grid-template-columns: 3vw 17vw 3vw 54vw 3vw;
     width:80vw;
     height:25vh;
     margin-top:5vh;
     background-color: white;
     border:solid 0.1vh black;
     border-radius: 20px;
+    margin-left:-0.1vh;
+}
+.ran{
+    position: absolute;
+    width:17vw;
+    grid-row: 2/3;
+    grid-column: 2/3;
+    text-align: center;
+    border-bottom: solid 3px grey;
+    bottom:0px;
+}
+.rank_text{
+    position: absolute;
+    width:54vw;
+    font-size: 2vh;
+    grid-row: 2/3;
+    grid-column: 4/5;
+    border-bottom: solid 3px grey;
+    bottom:0px;
+}
+.kan{
+    position: absolute;
+    width:17vw;
+    grid-row: 3/4;
+    grid-column: 2/3;
+    border-bottom: solid 3px grey;
+    bottom:0px;
+}
+.condition_text{
+    position: absolute;
+    width:54vw;
+    font-size: 2vh;
+    grid-row: 3/4;
+    grid-column: 4/5;
+    border-bottom: solid 3px grey;
+    bottom:0px;
+}
+.syou{
+    position: absolute;
+    width:17vw;
+    text-align: center;
+    grid-row: 4/5;
+    grid-column: 2/3;
+    top:2vh;
+}
+.message_text{
+    position: absolute;
+    font-size: 2vh;
+    grid-row: 4/5;
+    grid-column: 4/5;
+    top:2vh;
 }
 </style>
   
